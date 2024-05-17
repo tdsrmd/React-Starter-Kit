d99596750cd617afb2840daac3953acc36aae3dc
tdsrmd <demirsedatdemir@icloud.com>
2024-05-17 19:02:17 +0300

    Refactor route paths and implement lazy loading for improved performance

--- src/routes/auth-routes.tsx
+++ src/routes/auth-routes.tsx
@@ -1,17 +1,21 @@
+import { Suspense, lazy } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import { AuthLayoutPathPath, SignInPathPath } from './path'
 
 import AuthLayout from '@/pages/(auth)/layout'
-import SignInPage from '@/pages/(auth)/sign-in'
+
+const SignIn = lazy(() => import('@/pages/(auth)/sign-in'))
 
 export default function AuthRoutes() {
   return (
-    <Routes>
-      <Route path={AuthLayoutPathPath} element={<AuthLayout />}>
-        <Route index element={<SignInPage />} />
-        <Route path={SignInPathPath} element={<SignInPage />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path={AuthLayoutPathPath} element={<AuthLayout />}>
+          <Route index element={<SignIn />} />
+          <Route path={SignInPathPath} element={<SignIn />} />
+        </Route>
+      </Routes>
+    </Suspense>
   )
 }
--- src/routes/dashboard-routes.tsx
+++ src/routes/dashboard-routes.tsx
@@ -1,15 +1,19 @@
+import { Suspense, lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import DashboardLayout from '@/pages/(user)/layout'
-import HomePage from '@/pages/(user)/home'
 import { DashboardLayoutPathPath } from './path'
+
+const HomePage = lazy(() => import('@/pages/(user)/home'))
 
 export default function DashboardRoutes() {
   return (
-    <Routes>
-      <Route path={DashboardLayoutPathPath} element={<DashboardLayout />}>
-        <Route index element={<HomePage />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path={DashboardLayoutPathPath} element={<DashboardLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   )
 }
